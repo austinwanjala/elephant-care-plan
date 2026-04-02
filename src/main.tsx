f2738cc21632aa73325190e663492bf48b5db996
[dyad] <wanjalaaustine819@gmail.com>
2026-04-02 09:53:49 +0300

    Update 1 file

--- src/main.tsx
+++ src/main.tsx
@@ -2,4 +2,14 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
+const isLocalhost = window.location.hostname === "localhost" || window.location.hostname === "127.0.0.1";
+
+if (import.meta.env.PROD || !isLocalhost) {
+  console.log = () => {};
+  console.info = () => {};
+  console.warn = () => {};
+  console.error = () => {};
+  console.debug = () => {};
+}
+
 createRoot(document.getElementById("root")!).render(<App />);
